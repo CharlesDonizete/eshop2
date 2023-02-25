7adc2e5ed532b9cad1dd41e1c8dcc0ab970a51e6
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-02-25 10:28:30 -0300

    Total da Compra

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CartModel } from '../models/cart.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  cart$: Observable<CartModel>;
+  constructor(
+    private store: Store<CartModel>,
+    private alertCtrl: AlertController
+  ) {
+    this.cart$ = store.pipe(select('cart'));
+  }
 }
