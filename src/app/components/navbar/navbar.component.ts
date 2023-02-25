6edd82214b98d3c5aad26c8bd65d79b0106fc5bc
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-02-25 08:47:58 -0300

    Navbar - Parte 1

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CartModel } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  cart$: Observable<CartModel>;
+
+  constructor(private store: Store<CartModel>) {}
+
+  ngOnInit() {}
+}
